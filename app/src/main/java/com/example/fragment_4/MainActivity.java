8bf8ff051c6993e5bfb7d5daf3f39cb679f81f79
Chan Shiang-Yun <cloudfly4326@gmail.com>
2022-09-13 16:13:08 +0800

    1.add a new Tab in TabLayout. 2.change the background and title of ActionBar 3. change the color of Tab.

--- app/src/main/java/com/example/fragment_4/MainActivity.java
+++ app/src/main/java/com/example/fragment_4/MainActivity.java
@@ -1,23 +1,59 @@
 package com.example.fragment_4;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
 
     private ViewPagerAdapter pagerAdapter;
     private ViewPager2 viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setBackgroundDrawable(getDrawable(R.drawable.background));
+        actionbar.setTitle("TabLayout_demo");
+
+//        1. set fragment content : create ViewPagerAdapter(extends FragmentStateAdapter)
         pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(),getLifecycle());
+
+//        2. set fragment content on ViewPager2 widget.
         viewPager = (ViewPager2) findViewById(R.id.viewPager_id);
         viewPager.setAdapter(pagerAdapter);
+
+//        3. using TabLayoutMediator().attach() :
+//        combine TabLayout widget and ViewPager2 widget.
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout_id);
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position){
+                    case 0:
+                        tab.setText("Tab 1");
+                        break;
+                    case 1:
+                        tab.setText("Tab 2");
+                        break;
+                    case 2:
+                        tab.setText("Tab 3");
+                        break;
+                    case 3:
+                        tab.setText("Tab 4");
+                        break;
+                }
+            }
+        }).attach();
 
     }
 }
--- app/src/main/java/com/example/fragment_4/ViewPagerAdapter.java
+++ app/src/main/java/com/example/fragment_4/ViewPagerAdapter.java
@@ -1,37 +1,53 @@
 package com.example.fragment_4;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ViewPagerAdapter extends FragmentStateAdapter {
+    private final List<Fragment> fragList;
     private Fragment fragment;
 
     public ViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
         super(fragmentManager, lifecycle);
+        fragList = new ArrayList<Fragment>();
+        fragList.add(new Fragment_1());
+        fragList.add(new Fragment_2());
+        fragList.add(new Fragment_3());
+        fragList.add(new Fragment_4());
+        Log.d("main","ViewPagerAdapter"); //每次開啟都會建立一次，從背景回來也會進入
     }
 
     @NonNull
     @Override
     public Fragment createFragment(int position) { //建立Fragment
-        switch (position){
-            case 0:
-                fragment = new Fragment_1();
-                break;
-            case 1:
-                fragment = new Fragment_2();
-                break;
-            case 2:
-                fragment = new Fragment_3();
-                break;
-        }
-        return fragment;
+        fragment = fragList.get(position);
+//        switch (position){
+//            case 0:
+////                fragment = new Fragment_1();
+//                break;
+//            case 1:
+////                fragment = new Fragment_2();
+//                break;
+//            case 2:
+////                fragment = new Fragment_3();
+//                break;
+//        }
+        Log.d("main","createFragment");
+
+        return fragment;//每次開啟都會建立一次，從背景回來也會進入
     }
 
     @Override
     public int getItemCount() {//傳回 Fragment 數量
-        return 3;
+        Log.d("main","getItemCount");//每次
+        return 4;
     }
 }
